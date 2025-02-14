6ad23d3ae79ee1f6a50b50e4a5b2436279f62d67
aniket-8719 <aniket07013@gmail.com>
2025-02-14 23:21:53 +0530

    first commit of project

--- models/User.js
+++ models/User.js
@@ -1,17 +1,21 @@
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 
-const userSchema = new mongoose.Schema({
-  userName: { type: String, required: true, unique: true, trim: true },
-  fullName: { type: String, required: true },
-  email: { type: String, required: true, unique: true, trim: true },
-  password: { type: String, required: true, minlength: 6 },
-  gender: { type: String, required: true, enum: ["Male", "Female", "Other"] },
-  dateOfBirth: { type: Date, required: true },
-  country: { type: String, required: true },
-});
+const userSchema = new mongoose.Schema(
+  {
+    userName: { type: String, required: true, unique: true, trim: true },
+    fullName: { type: String, required: true },
+    email: { type: String, required: true, unique: true, trim: true },
+    password: { type: String, required: true, minlength: 6 },
+    gender: { type: String, required: true, enum: ["Male", "Female", "Other"] },
+    dateOfBirth: { type: Date, required: true },
+    country: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
-
+// Ensure index creation
+userSchema.index({ userName: 1 }, { unique: true });
 
 
 // JWT TOKEN
